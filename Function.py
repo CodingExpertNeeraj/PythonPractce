f961de9ccc6d50807b36ef3aaf63dbeb6debd544
Neeraj Tiwari <nt98924@gmail.com>
2022-07-08 21:04:13 +0530

    tupple method 's

--- Function.py
+++ Function.py
@@ -1,4 +1,4 @@
-def func():
+def message():
     print("Hello world this is my first function program in python")
 
-func()
+message()
---
+++ tuppleMethod.py
@@ -0,0 +1,6 @@
+t1=(11,32,53,46,432,215)
+print(len(t1)) #no of elements in a tupple
+print(max(t1))
+print(min(t1))
+print(sum(t1))
+print(t1.count(215))
