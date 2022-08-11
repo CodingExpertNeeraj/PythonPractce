477c5a1c75252b5d2b0b2295872f3e0fd7aa2d2c
Neeraj Tiwari <nt98924@gmail.com>
2022-08-11 19:27:29 +0530

    Indian flag

--- tuppleMethod.py
+++ tuppleMethod.py
@@ -1,6 +1,6 @@
-t1=(11,32,53,46,432,215)
+t1=(11,32,53,46,432,215,46,46)
 print(len(t1)) #no of elements in a tupple
 print(max(t1))
 print(min(t1))
 print(sum(t1))
-print(t1.count(215))
+print(t1.count(46))
