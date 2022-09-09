2a147a725730cbfbe66b347ca39ba8da730eaca7
Neeraj tiwari <nt98924@gmail.com>
2022-09-09 13:58:14 +0530

    replication opreator in python

--- ListOp.py
+++ ListOp.py
@@ -10,3 +10,10 @@ print(18 not in l1)
 l2 = [6,7,8,9]
 l3 = l1 + l2
 print(l3)
+# l3 = l1 +  10
+# print(l3)     through error
+
+l3 = l1+['welcome','guys']
+print(l3)
+# replication opreator
+print(l1 *3)
