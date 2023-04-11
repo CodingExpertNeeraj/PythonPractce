48d738ec16ddd5922c24c01f1bde4816ff438e60
Neeraj tiwari <nt98924@gmail.com>
2023-04-11 13:43:07 +0530

    tuple in python

--- ListAndTuples.py
+++ ListAndTuples.py
@@ -6,5 +6,11 @@
 myList = [2,5,7,"Neeraj","India",22,"9may",2000]
 print(myList)
 print(type(myList))
+print(myList[6])
 
 #tuple
+myTuple = (89,6,754,"tiwari","almora","i am tuple","hope you are getting","samjho bhai")
+print(myTuple)
+print(type(myTuple))
+print(myTuple[5])
+
